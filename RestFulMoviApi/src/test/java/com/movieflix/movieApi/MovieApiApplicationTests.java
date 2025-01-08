c848525358a444e2a8722d690cfa8ef3297b7adf
lokeshsharma123456 <lokeshsharma123456@gmail.com>
2025-01-09 00:36:42 +0530

    New files

---
+++ RestFulMoviApi/src/test/java/com/movieflix/movieApi/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.movieflix.movieApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
